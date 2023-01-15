3a071d9dbdc2dfa60e43138c58c04b00c6ea26b6
Romain Cognard <romain@mynetwk.com>
2023-01-15 09:58:22 +0100

    Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let baseUrl = "http://localhost:3000/api"
+let products;
+
+function getProducts() {
+  fetch(`${baseUrl}/products`)
+    .then(response => response.json())
+    .then(data => {
+      products = data;
+      console.log(products);
+      products.forEach(product => {
+        console.log(product);
+      });
+    })
+    .catch(error => console.log(error));
+}
