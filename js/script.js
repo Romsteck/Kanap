ca986f5c7f39c50c14a97f0b767117b7d42f1c0b
Romsteck <88426041+Romsteck@users.noreply.github.com>
2023-01-18 13:59:21 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,3 +13,25 @@ function getProducts() {
     })
     .catch(error => console.log(error))
 }
+
+
+
+/*
+
+let baseUrl = "http://localhost:3000/api/products"
+
+fetch(`${baseUrl}`)
+  .then(response => response.json())
+  .then(products => {
+    console.log(products);
+    getProducts(products)
+  })
+  .catch(error => console.log(error))
+
+function getProducts(products) {
+    products.forEach(product => {
+      console.log(product)
+    });
+}
+
+*/
